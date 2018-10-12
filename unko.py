06c7f3346495e9d50f1356d27c9360464491cfb3
tkwk0331 <kz.tsukawaki@gmail.com>
2018-10-12 10:08:27 +0900

    add new file

---
+++ unko.py
@@ -0,0 +1,8 @@
+sudo apt install python3-pip
+pip3 install pymysql
+pip3 install django
+pip3 install django-bootstrap4
+pip3 install django-crispy-forms
+pip3 install django-pure-pagination
+pip3 install django-filters
+pip3 install django-filter
